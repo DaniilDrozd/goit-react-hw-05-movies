b39e42e300f2002b62f7439ea89a285d7ce2f7e8
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-09-17 13:49:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename="/goit-react-hw-05-movies">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
